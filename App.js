cbaf7e33bab4a5c5d8cec7d69461bb008783d629
cielcs <shotalicewww2@gmail.com>
2024-03-09 21:17:51 +0900

    modified bugs

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import {
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import "react-native-gesture-handler";
+import { v4 as uuidv4 } from "uuid";
+import { getRandomBase64 } from "react-native-get-random-values";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -22,17 +24,26 @@ import ScreenTmp from "./Screen/ScreenTmp";
 const Stack = createStackNavigator();
 
 const App = () => {
+  const generateRandomString = (length) => {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  };
   const [screens, setScreens] = useState([
-    { id: "A", title: "AAtitle" },
-    { id: "B", title: "BB" },
-    { id: "C", title: "CC" },
+    { id: generateRandomString(10), title: "ベンチプレス" },
+    { id: generateRandomString(10), title: "pull up" },
   ]);
 
-  // アプリ起動時に保存されたデータを読み込む
+  // アプリ起動時に保存されたデータを読み込む;
   useEffect(() => {
     loadScreens();
   }, []);
-  // スクリーンの読み込み
+  // スクリーンの読み込み;
   const loadScreens = async () => {
     try {
       const storedScreens = await AsyncStorage.getItem("screens");
@@ -43,9 +54,10 @@ const App = () => {
       console.error("Error loading screens", error);
     }
   };
+  // 指定された桁数のランダムな文字列を生成する関数
 
   const addScreen = async (title) => {
-    const newScreenId = String.fromCharCode(65 + screens.length);
+    const newScreenId = generateRandomString(10);
     const newScreen = { id: newScreenId, title: title };
     const updatedScreens = [...screens, newScreen];
     setScreens(updatedScreens);
@@ -59,6 +71,7 @@ const App = () => {
 
   const deleteScreen = async (screenId) => {
     const updatedScreens = screens.filter((screen) => screen.id !== screenId);
+    console.log(updatedScreens);
     setScreens(updatedScreens);
     try {
       await AsyncStorage.setItem("screens", JSON.stringify(updatedScreens));
@@ -71,7 +84,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="ScreenHome" component={ScreenHome} />
-        {/* <Stack.Screen name="ScreenMenu" component={ScreenMenu} /> */}
         <Stack.Screen name="ScreenMenu" options={{ title: "ScreenMenu" }}>
           {(props) => (
             <ScreenMenu
@@ -85,7 +97,7 @@ const App = () => {
         {screens.map((screen) => (
           <Stack.Screen
             key={screen.id}
-            name={screen.id}
+            name={screen.id} // 一意の名前に変更する
             options={{ title: screen.title }}
           >
             {(props) => <ScreenTmp {...props} screenId={screen.id} />}
